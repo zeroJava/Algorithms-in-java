c20d83c7ff4026bc2482da0e7059e5ee05bfce66
zero <zero_2009@hotmail.co.uk>
2015-05-11 05:03:45 +0100

    sorting finished

--- src/sortExercise/Insersort1.java
+++ src/sortExercise/Insersort1.java
@@ -1,37 +1,64 @@
 package sortExercise;
 
+import java.util.Scanner;
+
 public class Insersort1 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int size = input.nextInt();
+		int[] array = new int[size];
+		
+		for(int index = 0; index < size; index++)
+		{
+			array[index] = input.nextInt();
+		}
+		
+		insertIntoSorted(array);
+		input.close();
 	}
 
 	public static void insertIntoSorted(int[] ar)
 	{
 		int lastIndex = ar.length - 1;
 		int usortedValue = ar[lastIndex];
+		boolean nothingHappen = true;
 
-		for (int i = lastIndex; i >= 0; i--)
+		for (int i = lastIndex; i > -1; i--)
 		{
 			if (ar[i] == usortedValue)
 			{
 				continue;
 			} 
-			else if (usortedValue < ar[i] && ar[i] > ar[i - 1])
+			else if (ar[i] > usortedValue)
 			{
 				ar[i + 1] = ar[i];
-				System.out.println(ar[0] + " " + ar[1] + " " + ar[2] + " "
-						+ ar[3] + " " + ar[4]);
+				printArray(ar);
 			} 
-			else if (usortedValue > ar[i])
+			else if (ar[i] < usortedValue)
 			{
-				ar[i] = 3;
-				System.out.println(ar[0] + " " + ar[1] + " " + ar[2] + " "
-						+ ar[3] + " " + ar[4]);
+				System.out.println("check " + ar[i] + " " + ar[i + 1]);
+				ar[i + 1] = usortedValue;
+				nothingHappen = false;
+				System.out.println("check " + ar[i] + " " + ar[i + 1]);
+				break;
 			}
 		}
+		
+		if (nothingHappen == true)
+		{
+			ar[0] = usortedValue;
+		}
+		printArray(ar);
 	}
 
+	private static void printArray(int[] ar)
+	{
+		for (int n : ar)
+		{
+			System.out.print(n + " ");
+		}
+		System.out.println("");
+	}
 }
