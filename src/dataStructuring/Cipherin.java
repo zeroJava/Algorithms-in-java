efcda0ad8c358179474c843842ac75ca0a81265e
zero <zero_2009@hotmail.co.uk>
2015-05-16 23:16:56 +0100

    Some stuff

---
+++ src/dataStructuring/Cipherin.java
@@ -0,0 +1,36 @@
+package dataStructuring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cipherin {
+
+	public static List<Character> deChipherNumber(int item)
+	{
+		List<Character> covertedValue = new ArrayList<Character>();
+		String[] numbers = Integer.toString(item).split("");
+		int size = numbers.length;
+		
+		for(int index = 0; index < size; index++)
+		{
+			char letter = letters(Integer.parseInt(numbers[index]));
+			//Character leter = letter;
+			covertedValue.add(letter);
+		}
+		return covertedValue;
+	}
+	
+	private static char letters(int number)
+	{
+		switch (number)
+		{
+		case 0:
+			return ' ';
+		case 1:
+			return 'A';
+		default:
+			break;
+		}
+		return 0;
+	}
+}
