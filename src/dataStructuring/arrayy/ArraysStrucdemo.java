56dc400d47f2a2d4dae557e672950116cee5807f
zero <zero_2009@hotmail.co.uk>
2015-05-31 03:04:04 +0100

    Revert "newst"
    This reverts commit 03bcf035c0169a46f67247b6b95d42d2317a4218.

--- src/dataStructuring/arrayy/ArraysStrucdemo.java
+++ src/dataStructuring/arrayy/ArraysStrucdemo.java
@@ -7,11 +7,6 @@ public class ArraysStrucdemo {
 		Arraystruc arraystruc = new Arraystruc();
 		arraystruc.linearSearch(5, arraystruc.array3);
 		arraystruc.binarySearch(48, arraystruc.array3);
-		int[][] array = { { 1, 5, 14, 82 }, { 63, 43, 48, 37 },
-				{ 71, 31, 72, 124 } };
-
-		int[][] array2 = { { 458, 34, 214, 1 }, { 3282, 347, 92145, 634 },
-				{ 6547, 5, 2, 3 } };
-		arraystruc.matrixMultipication(array, array2);
+		arraystruc.giveValue();
 	}
 }
